27c229e9bec3226c897c19edd7b65204ab32d167
AroshXR <aroshanasandeep454@gmail.com>
2026-03-04 15:33:11 +0530

    initial commit

---
+++ src/test/java/com/medisphere/patient/MedispherePatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medisphere.patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedispherePatientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
