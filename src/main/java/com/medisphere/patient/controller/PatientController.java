afe5a453ddd6a1dab4aed7868a37f081b2ee33e4
DasunD2002 <dasun5967@gmail.com>
2026-04-07 01:43:21 +0530

    Initialize patient service file structure

---
+++ src/main/java/com/medisphere/patient/controller/PatientController.java
@@ -0,0 +1,13 @@
+package com.medisphere.patient.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/patient/api/v1")
+@RequiredArgsConstructor
+public class PatientController {
+}
---
+++ src/main/java/com/medisphere/patient/repository/PatientRepository.java
@@ -0,0 +1,4 @@
+package com.medisphere.patient.repository;
+
+public interface PatientRepository {
+}
